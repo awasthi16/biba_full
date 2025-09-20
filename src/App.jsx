e8d0e3286bbf3803ff96c80e53f48dd69c41e1b3
sharad kushwah <technicalsahilkushwah@gmail.com>
2025-09-20 16:45:57 +0530

    frnj

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Footer'
+import Home from './Home'
 
 
 const App = () => {
   return (
     <>
-    
+    <Home/>
     <Footer/>
    
     </>
---
+++ src/Home.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <>
+    <section className='home'>
+        <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw5f17d132/A-AW25/aw25-sept19.jpg" alt="" width="100%" />
+    </section>
+
+    <section className='home2'>
+        <div className="cards">
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw84967720/A-AW25/kritiremove/b-main.jpg" alt="" />
+        </div>
+
+        <div className="cards">
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw842beec0/A-AW25/kritiremove/b1.jpg" alt="" />
+        </div>
+
+        <div className="cards">
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dwd4dc1155/A-AW25/kritiremove/b2.jpg" alt="" />
+        </div>
+
+        <div className="cards">
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dwa07aea2a/A-AW25/kritiremove/b4.jpg" alt="" />
+        </div>
+
+        <div className="cards">
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw6973a312/A-AW25/kritiremove/b5.jpg" alt="" />
+        </div>
+    </section>
+
+    <section className='home3'>
+        <div className="cards3">
+            <img src="https://t3.ftcdn.net/jpg/01/43/75/28/360_F_143752895_VEjQuZnOLV5sWqeV11m88xWSSW7Dihp4.jpg" alt="" />
+        </div>
+
+        <div className="cards3">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1posIsUfBg6A86FDssblkgkhp_I3IbNEKRL9FsyyzaUpPy8SEZXg8nLqVkpdphzEKGQc&usqp=CAU" alt="" />
+        </div>
+
+        <div className="cards3">
+            <img src="https://safaaworld.com/cdn/shop/articles/Blog-Image_b21adffc-7377-40da-963c-8d43a00cd714.jpg?v=1726489198" alt="" />
+        </div>
+    </section>
+
+    <section className='home4'>
+
+        <div className="cards3">
+            <img src="https://www.hatkay.com/cdn/shop/articles/Style-Tips-to-Follow-When-You-Are-Wearing-Traditional-Indian-Clothes.jpg?v=1662208510" alt="" />
+        </div>
+
+        <div className="cards3">
+            <img src="https://www.theknot.com/tk-media/images/da32b3c3-1e74-4ee6-bc14-39bee75672c9.jpg" alt="" />
+        </div>
+
+        <div className="cards3">
+            <img src="https://www.mohifashion.com/cdn/shop/articles/BB_216433_Final_320eb394-2ae4-42e1-91f1-7e342fc976df.jpg?v=1718363593&width=1100" alt="" />
+        </div>
+    </section>
+
+
+    <main className='home5'>
+        <div className='cards4'>
+            <img src="https://images.biba.in/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dwae5fb86e/A-AW25/festive/red-sept17.jpg" alt="" />
+        </div>
+
+    </main>
+
+    <section className='text-home'>
+        <h4>EXPLOSE COLLECTION</h4>
+    </section>
+
+    <section className='home6'>
+        <div className="cards5">
+            <img src="https://images.biba.in/dw/image/v2/BKQK_PRD/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dwf7858996/A-EOSS/posh-june7.jpg" alt="" />
+        </div>
+
+        <div className="cards5">
+            <img src="https://images.biba.in/dw/image/v2/BKQK_PRD/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw97a056f3/A-EOSS/indigo-june7.jpg" alt="" />
+        </div>
+
+        <div className="cards5">
+            <img src="https://images.biba.in/dw/image/v2/BKQK_PRD/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dwcca13d61/A-EOSS/jewellery-june7.jpg" alt="" />
+        </div>
+
+        <div className="cards5">
+            <img src="https://images.biba.in/dw/image/v2/BKQK_PRD/on/demandware.static/-/Library-Sites-BibaSharedLibrary/en_IN/dw30bf3ab3/A-EOSS/luxury-june7.jpg" alt="" />
+        </div>
+    </section>
+
+    <main className='email'>
+        <div className='entre'>
+            <h3>BE A PART OF BIBA FAMILY</h3>
+            <p>Keep yourself updated with style tips & more</p>
+        </div>
+
+        <div className="search">
+            <i className='fa fa-search'></i>
+            <input type="text" placeholder='ENTRE EMAIL'/>
+        </div>
+    </main>
+    </>
+  )
+}
+
+
+export default Home
