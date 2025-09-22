0598201a929e87b68e395cbb408dc1ba4d51f3da
rani rathor <raniratnor135@gmail.com>
2025-09-22 23:45:30 +0530

    ranii

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Footer'
+import Girls from './Girls'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
     
     <Footer/>
+    <Girls/>
    
     </>
   )
---
+++ src/Girls.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Girls.css'
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+
+const Girls = () => {
+  return (
+    <>
+    
+    <div className="table1">
+
+        <div className="table2">
+            <div className="hom">HOME / GIRLS</div>
+                
+            
+
+
+        </div>
+
+
+    </div>
+    
+    
+    
+    </>
+  )
+}
+
+export default Girls
